c30f3955ceaa8d8083a22111337ffa1e39d3bf61
FumoRin <edu.dimasfaizsatria@gmail.com>
2025-03-10 10:29:58 +0700

    Fixing cors allow origin error

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 // Routes
-app.use('/events', require('./routes/eventRoutes'));
-app.use('/users', require('./routes/userRoutes'));
-app.use('/auth', require('./routes/authRoutes')); // Tambahkan 
+app.use("/events", require("./routes/eventRoutes"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/auth", require("./routes/authRoutes")); // Tambahkan
 
 module.exports = app;
